ba982173aa06a7ff597f5a824a6e535e4480593f
Tina <skielred@gmail.com>
2018-04-24 01:01:06 +0200

    main, initial types and keywords

---
+++ french.h
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2018
+** french.h
+** File description:
+** french.h
+*/
+
+#pragma once
+
+#define principal main
+
+#define entier int
+#define lettre char
+#define néant void
+#define nonsigné unsigned
+#define taille_t size_t
+#define tailles_t ssize_t
+#define FICHIER FILE
+#define NUL NULL
+
+#define tantque while
+#define pour for
+#define si if
+#define sinon else
+#define et &&
+#define ou ||
+#define renvoie return
+
